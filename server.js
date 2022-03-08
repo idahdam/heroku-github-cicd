8d43576767edd04c73737dcc2f9f84eb9ad044c7
nasshitell@gmail.com <nasshitell@gmail.com>
2022-03-08 23:38:43 +0700

    fixing heroku index

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`This app running on http://localhost:${PORT}!`)
+})
 module.exports = app
