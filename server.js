3b9895e9c280d6104f8173643ef6d1d00961f799
nasshitell@gmail.com <nasshitell@gmail.com>
2022-03-08 23:48:53 +0700

    Fixing

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
 const {greeting} = require('./user')
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
-})
-
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log(`This app running on http://localhost:${PORT}!`)
 })
 module.exports = app
